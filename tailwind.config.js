2e85c02367c25389482f4a5293a6b3b896e34baa
Maryna <marimariovod@gmail.com>
2025-01-08 19:45:56 +0200

    add tailwind and fonts
    add root colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./src/**/*.{html,js}", "./*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      nunito: ["Nunito", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        backgroundMain: "#fdfeff",
+        backgroundSecondary: "#eceff6",
+        blackText: "#333",
+        btn: "#bfdef5",
+        text: "#288dd5",
+        whiteText: "#ecf0f6",
+        iconHover: "#436b88",
+      },
+    },
   },
   plugins: [],
 };
